1ee9cfd61bfebf96619826e794e499b55d88b674
AdminGit2024 <haraledeepak6@gmail.com>
2024-09-10 19:27:15 +0530

    I have created loginPage and LoginTestPage classes with code.

--- src/test/java/com/jala/qa/testpages/HomeTestPage.java
+++ src/test/java/com/jala/qa/testpages/HomeTestPage.java
@@ -4,21 +4,36 @@ import java.io.IOException;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import com.jala.qa.pom.pages.HomePage;
 import com.jala.qa.testbase.TestBase;
+import com.jala.qa.utility.utilityClass;
 
-public class HomeTestPage extends TestBase{
+public class HomeTestPage extends TestBase {
+	HomePage home;
+	utilityClass util;
 
 	public HomeTestPage() throws IOException {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@BeforeMethod
-	public void setup() {
+	public void setup() throws IOException {
 		Initialization();
+
+		home = new HomePage();
+		util = new utilityClass();
+		util.LoginFunctionality(prop.getProperty("UserName"), prop.getProperty("Password"));
 	}
-	
+
+	@Test
+	public void clickOncreateTab() {
+		home.clickOnEmployeTab();
+		home.clicOnCreateTab();
+	}
+
 	@AfterMethod
 	public void tearDown() {
 		driver.close();
--- src/test/java/com/jala/qa/utility/utilityClass.java
+++ src/test/java/com/jala/qa/utility/utilityClass.java
@@ -1,5 +1,20 @@
 package com.jala.qa.utility;
 
+import java.io.IOException;
+
+import com.jala.qa.pom.pages.LoginPage;
+
 public class utilityClass {
+	
+	
+//	************* Login Functinlity *******************
+	
+	public void LoginFunctionality(String umane, String pass) throws IOException {
+		LoginPage login = new LoginPage();
+		login.enterUserName(umane);
+		login.enterPassword(pass);
+		login.clickOnLoginBtn();
+	}
+	
 
 }
