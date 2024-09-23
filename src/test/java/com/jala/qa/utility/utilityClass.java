422add08aacb5c743c8eba12c657a9af06897e6d
AdminGit2024 <haraledeepak6@gmail.com>
2024-09-23 19:34:46 +0530

    Framework part has been completed

--- src/test/java/com/jala/qa/utility/utilityClass.java
+++ src/test/java/com/jala/qa/utility/utilityClass.java
@@ -1,20 +1,36 @@
 package com.jala.qa.utility;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.io.FileHandler;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.jala.qa.pom.pages.LoginPage;
+import com.jala.qa.testbase.TestBase;
 
-public class utilityClass {
+import net.bytebuddy.utility.RandomString;
+
+public class utilityClass extends TestBase{
 	
 	
-//	************* Login Functinlity *******************
+public utilityClass() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	//	************* Login Functinlity *******************
 //	@DataProvider(name="exceldata")
 	public void LoginFunctionality(String umane, String pass) throws IOException {
 		LoginPage login = new LoginPage();
@@ -43,5 +59,60 @@ public class utilityClass {
 		return data;
 	}
 	
+//	***************************** screenShot functionlaity ************************
+	
+	public void captureScreenShot(String methodName) throws IOException {
+		// step1: convert webdriver object to TakesScreenshot interface
+		TakesScreenshot screenshot = ((TakesScreenshot) driver);
+
+		// step2: call getScreenshotAs method to create image file
+
+		File src = screenshot.getScreenshotAs(OutputType.FILE);
+		String random = RandomString.make(4);
+		File dest = new File("C:\\Users\\OM SAI AM\\eclipse-workspace\\Jala_HybridFramwork_Sept2024_Batch_7PM\\screenshot\\"+methodName+random+".png");
+
+		// step3: copy image file to destination
+//		FileUtils.copyFile(src, dest);
+		FileHandler.copy(src, dest);
+
+		
+	}
+	
+	
+	
+//	 ********************* extent screenshot code ****************
+	
+	
+public static String takeScreenshot(String testName) {
+		
+		File sourceScreenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		File destinationScreenshotFile = new File(System.getProperty("user.dir")+"\\Screenshots\\"+testName+".png");
+		try {
+//			FileUtils.copyFile(sourceScreenshotFile, destinationScreenshotFile);
+			FileHandler.copy(sourceScreenshotFile, destinationScreenshotFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return destinationScreenshotFile.getAbsolutePath();
+	
+	}
+	
+//	###################### extent report ######################
+	
+	public static ExtentReports getExtentReport() {
+		
+		ExtentReports report = new ExtentReports();
+		
+		File extentReportFile = new File(System.getProperty("user.dir")+"\\ExtentReports\\eReport.html");
+		
+		ExtentSparkReporter sparkReporter = new ExtentSparkReporter(extentReportFile);
+		
+		report.attachReporter(sparkReporter);
+		
+		return report;
+		
+	}
 	
 }
